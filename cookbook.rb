81f91dafdfca09548f18b1aa6a650f82fe2c6375
José C. Messias <josecmessias22@gmail.com>
2023-02-02 19:35:44 -0300

    Boas práticas

--- cookbook.rb
+++ cookbook.rb
@@ -1,19 +1,21 @@
+CADASTRAR_RECEITA = 1
+VISUALIZAR_RECEITA = 2
+BUSCAR_RECEITA = 3
+SAIR = 4
+
 def bem_vindo()
     puts "Bem-vindo ao Cookbooks, sua rede social de receitas"
 end
 
-def agradecimento()
-    puts
-    puts "Obrigado por usar o Cookbook"
-end
-
 def menu()
-    puts "[1] Cadastrar uma receitas"
-    puts "[2] Ver todas as receitas"
-    puts "[3] Sair"
+    puts "[#{CADASTRAR_RECEITA}] Cadastrar uma receitas"
+    puts "[#{VISUALIZAR_RECEITA}] Ver todas as receitas"
+    puts "[#{BUSCAR_RECEITA}] Buscar receitas"
+    puts "[#{SAIR}] Sair"
     
     print "Escolha uma opção: "
     return gets.to_i()
+    puts
 end
 
 def inserir_receitas()
@@ -28,16 +30,24 @@ def inserir_receitas()
     return { nome: nome, tipo: tipo }
 end
 
-def imprimir_receitas(array_receitas)
+def imprimir_receitas(array_imprimir_receitas)
     puts "======= Receitas Cadastradas ======="
-        array_receitas.each do |receita|
-            puts "#{receita[:nome]} - #{receita[:tipo]}"
-        end   
-        puts
+    array_imprimir_receitas.each do |receita|
+        puts "#{receita[:nome]} - #{receita[:tipo]}"
+    end   
+    puts
+    if(array_imprimir_receitas.empty?)
+        puts "Nenhuma receita cadastrada"
+    end
 end
 
-def erro_opcao()
-    puts "Opção invalida!" 
+def buscar_receita(array_buscar_receitas)
+    puts
+    print "Insira o nome da receita: "
+    numero_receita = gets.to_i()
+    puts array_buscar_receitas[numero_receita]
+    puts
+    end
 end
 
 bem_vindo()
@@ -46,15 +56,22 @@ receitas = []
 
 opcao = menu()
 
-while(opcao != 3 ) do 
-    if(opcao == 1)
+loop do 
+    if(opcao == CADASTRAR_RECEITA)
         receitas << inserir_receitas()
-    elsif(opcao == 2)
+    elsif(opcao == VISUALIZAR_RECEITA)
         imprimir_receitas(receitas)
+    elsif(opcao == BUSCAR_RECEITA)
+        buscar_receita(receitas)
+    elsif(opcao != SAIR)
+        break
     else
-        erro_opcao()
+        puts "Opção invalida!" 
     end
+
     puts
     opcao = menu()
 end
-agradecimento()
+
+puts
+puts "Obrigado por usar o Cookbook"
