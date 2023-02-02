d0691aa8161f80e44718f5df6cd5033acd02a5e0
José C. Messias <josecmessias22@gmail.com>
2023-02-02 17:41:06 -0300

    Métodos

--- cookbook.rb
+++ cookbook.rb
@@ -1,44 +1,60 @@
-puts "Bem-vindo ao Cookbooks, sua rede social de receitas"
+def bem_vindo()
+    puts "Bem-vindo ao Cookbooks, sua rede social de receitas"
+end
 
-receitas = []
-
-puts "[1] Cadastrar uma receitas"
-puts "[2] Ver todas as receitas"
-puts "[3] Sair"
-
-print "Escolha uma opção: "
-opcao = gets.to_i()
-
-while(opcao != 3 ) do 
-
-    if(opcao == 1)
-        print "Digite o nome da receita: "
-        nome = gets.chomp()
-        print "Digite o tipo da receita: "
-        tipo = gets.chomp()
-
-        receitas << { nome: nome, tipo: tipo }
-        puts
-        puts "Receita #{nome} cadastrada com sucesso!"
-        puts
-    elsif(opcao == 2)
-        puts "======= Receitas Cadastradas ======="
-        receitas.each do |receita|
-            puts "#{receita[:nome]} - #{receita[:tipo]}"
-        end   
-        puts
-    else
-        puts "Opção invalida!"   
-    end
-    
+def agradecimento()
     puts
+    puts "Obrigado por usar o Cookbook"
+end
+
+def menu()
     puts "[1] Cadastrar uma receitas"
     puts "[2] Ver todas as receitas"
     puts "[3] Sair"
-
+    
     print "Escolha uma opção: "
-    opcao = gets.to_i()
+    return gets.to_i()
 end
 
-puts
-puts "Obrigado por usar o Cookbook"
+def inserir_receitas()
+    print "Digite o nome da receita: "
+    nome = gets.chomp()
+    print "Digite o tipo da receita: "
+    tipo = gets.chomp()
+  
+    puts
+    puts "Receita #{nome} cadastrada com sucesso!"
+    puts
+    return { nome: nome, tipo: tipo }
+end
+
+def imprimir_receitas(array_receitas)
+    puts "======= Receitas Cadastradas ======="
+        array_receitas.each do |receita|
+            puts "#{receita[:nome]} - #{receita[:tipo]}"
+        end   
+        puts
+end
+
+def erro_opcao()
+    puts "Opção invalida!" 
+end
+
+bem_vindo()
+
+receitas = []
+
+opcao = menu()
+
+while(opcao != 3 ) do 
+    if(opcao == 1)
+        receitas << inserir_receitas()
+    elsif(opcao == 2)
+        imprimir_receitas(receitas)
+    else
+        erro_opcao()
+    end
+    puts
+    opcao = menu()
+end
+agradecimento()
